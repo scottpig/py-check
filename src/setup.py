33840076609294baffbae901b4ecb95c80091941
Scott Pigman <scott@Jazzmaster.local>
2013-01-21 21:49:56 -0500

    Updated metadata. Version = 0.1

--- src/setup.py
+++ src/setup.py
@@ -6,9 +6,20 @@ Created on Jan 13, 2013
 from distutils.core import setup
 
 setup(
-    name='py-check',
-    version='0.1dev',
-    packages=['pycheck',],
-    license='MIT',
-    long_description=open('README.txt').read(),
+    name='pycheck',
+    version='0.1',
+    description='Type checking tools for debugging and developing Python',
+    packages=['pycheck', 'pycheck.test'],
+    classifiers=[
+                 'Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development',
+                 'Intended Audience :: Developers'
+                ],
+    long_description=open('README.TXT').read(),
+    author='Scott Pigman',
+    author_email='scott.pigman@gmail.com',
+    
 )
